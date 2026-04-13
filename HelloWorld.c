3516449d413ff547d683f48503cb29e509ba8750
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-04-13 06:13:27 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int a, b, c;
+  printf("Please enter value of x : ");
+  scanf("%d",&a);
+  printf("\nPlease enter value of y :");
+  scanf("%d", &b);
+  c=a+b;
+  printf("\nSum of %d and %d = %d", a,b,c);
+}
